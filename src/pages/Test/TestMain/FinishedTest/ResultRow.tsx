b102f9aa0b44e82f02624b47ad39ca8efc02e812
Benjamin Hall <benjaminjahall@gmail.com>
2025-03-12 12:22:00 -0400

    Add link to observation in results

--- src/pages/Test/TestMain/FinishedTest/ResultRow.tsx
+++ src/pages/Test/TestMain/FinishedTest/ResultRow.tsx
@@ -5,23 +5,22 @@ import {
   Card,
   CardContent,
   CardMedia,
+  IconButton,
   Stack,
   Typography,
 } from "@mui/material";
 import { Observation } from "src/services/inaturalist/Api";
 import { TestAnswer } from "../types";
 import { imageUrl } from "src/utils/imageUrl";
+import { LinkOutlined } from "@mui/icons-material";
 
-export default function ResultRow({
-  observation,
-  answer,
-}: ResultRowProps) {
+export default function ResultRow({ observation, answer }: ResultRowProps) {
   const rating = TestAnswer.scoreRating(answer.score);
   const alertColors: Record<typeof rating, AlertColor> = {
     bad: "error",
     ok: "warning",
-    good: "success"
-  }
+    good: "success",
+  };
 
   const url = observation.photos?.[0].url;
   const newUrl = url ? imageUrl(url, "medium") : undefined;
@@ -60,15 +59,24 @@ export default function ResultRow({
           justifyContent="space-between"
         >
           <Box>
-            <Typography variant="h5">{observation.taxon?.name}</Typography>
+            <Stack
+              direction="row"
+              spacing="4px"
+              justifyContent="start"
+              alignItems="center"
+            >
+              <Typography variant="h5">{observation.taxon?.name}</Typography>
+              {observation.uri && (
+                <IconButton href={observation.uri} target="_blank" size="small">
+                  <LinkOutlined />
+                </IconButton>
+              )}
+            </Stack>
             <Typography variant="subtitle1">
               {observation.taxon?.preferred_common_name || "No common name"}
             </Typography>
           </Box>
-          <Alert
-            variant="standard"
-            severity={alertColors[rating]}
-          >
+          <Alert variant="standard" severity={alertColors[rating]}>
             <Typography variant="body1">
               Guess: {answer.guess || "Skipped"}
             </Typography>
