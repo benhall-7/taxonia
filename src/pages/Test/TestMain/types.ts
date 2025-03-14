b58d7e28f63a61283c66f72c955164d7d498ad14
Benjamin Hall <benjaminjahall@gmail.com>
2025-03-14 14:42:17 -0400

    Rework scoring to treat words individually

--- src/pages/Test/TestMain/types.ts
+++ src/pages/Test/TestMain/types.ts
@@ -8,29 +8,46 @@ export type TestAnswer = {
 
 export const TestAnswer = {
   score(observation: Observation, guess?: string): TestAnswer {
-    const unlerped =
-      1 - normalizedLevenshtein(observation.taxon?.name || "", guess || "");
+    const normalizedA = observation.taxon?.name?.trim().toLocaleLowerCase();
+    const normalizedB = guess?.trim().toLocaleLowerCase();
 
-    const lerped =
-      unlerped < 0.5
-        ? 0
-        : unlerped > 0.95
-          ? 1
-          : (unlerped - 0.5) / (0.95 - 0.5);
+    const wordsA = normalizedA?.split(" ") || [];
+    const wordsB = normalizedB?.split(" ") || [];
+
+    const count = Math.max(wordsA.length, wordsB.length);
+
+    const range = Array.from({ length: count }, (_, index) => index);
+    const scores = range.map((index) => {
+      const wordA = wordsA[index];
+      const wordB = wordsB[index];
+
+      const unlerped = 1 - normalizedLevenshtein(wordA, wordB);
+      const lerped =
+        unlerped < 0.5
+          ? 0
+          : unlerped > 0.9
+            ? 1
+            : (unlerped - 0.5) / (0.9 - 0.5);
+
+      return lerped;
+    });
+
+    const averageScore =
+      scores.reduce((acc, score) => acc + score, 0) / scores.length;
 
     return {
       guess,
-      score: lerped,
+      score: averageScore,
     };
   },
   scoreRating(score: number) {
-    if (score < 0.75) {
-      return "bad"
+    if (score < 0.5) {
+      return "bad";
     }
     if (score < 0.9) {
-      return "ok"
+      return "ok";
     }
-    return "good"
+    return "good";
   },
   default(): TestAnswer {
     return {
